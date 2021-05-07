29e8eda482fd3fdbf5c12702bada4b2197a32676
Carolin Benjamins <benjamin@tnt.uni-hannover.de>
2021-05-07 10:54:51 +0200

    added newlines between pre-, main- and post-command. added documentation

--- slurmbuilder/slurmbuilder.py
+++ slurmbuilder/slurmbuilder.py
@@ -5,6 +5,30 @@ import itertools
 
 
 class SlurmBuilder(object):
+    """
+    Build slurm files containing commands and slurm options.
+
+    The slurm bash files are save in `runscript_outdir`. In this directory a file with name `runcommands.sh` is generated.
+    It contains the commands to start each generated bash file. The slurm jobs can thus be started as follows for all
+    generated files: `bash runcommands.sh`.
+
+    A generated bash file can look as follows::
+
+    #!/bin/bash
+    #
+    #SBATCH --mail-user=some_mail@bubbib.com
+    #SBATCH --mail-type=ALL
+    #SBATCH --partition=cpu_normal
+    #SBATCH --job-name=my_slurm_job_s6_neps16
+    #SBATCH --time=48:00:00
+    #SBATCH --mem-per-cpu=1000
+    #
+    echo precommand
+    echo basecommand: Hello World --seeds 6 --num_episodes 16
+    echo postcommand
+
+
+    """
     def __init__(
             self,
             job_name: str,
@@ -14,12 +38,54 @@ class SlurmBuilder(object):
             post_command: str = "",
             partition: str = "cpu_normal",
             time: str = "48:00:00",
-            mem_per_cpu: str = "1000",
+            mem_per_cpu: str = "1000M",
             mail_type: str = "ALL",
             runscript_outdir: str = "runscripts/generated",
             iteration_list: list = [],
-            **kwargs
     ):
+        """
+        Get information.
+        For more information on slurm parameters see the
+        `slurm wiki page <https://tntintern/wikitnt/index.php/TNT_Cluster_%C3%9Cbersicht>`.
+
+        Parameters
+        ----------
+        job_name : str
+            Job name for slurm. Can be appended by ids from `iteration_list`.
+        mail_user : str
+            E-mail address from user.
+        base_command : str
+            Command which should be executed. Appended with names and values from `iteration_list`.
+        pre_command : str, default=""
+            Everything that should be executed before `base_command`. Should contain intermediate newlines if applicable.
+        post_command : str, default=""
+            Everything that should be executed after `base_command`. Should contain intermediate newlines if applicable.
+        partition : str, default="cpu_normal"
+            Slurm partition to start jobs on. See the `slurm wiki page <https://tntintern/wikitnt/index.php/TNT_Cluster_%C3%9Cbersicht>`
+            for more information about available partitions.
+        time : str, default="48:00:00"
+            Alloted runtime in format HH:MM:SS.
+        mem_per_cpu : str, default="1000M"
+            Memory per cpu.
+        mail_type : str, default="ALL"
+            What notifications to get from slurm via e-mail.
+        runscript_outdir : str, default="runscripts/generated"
+            Where to save the slurm scripts.
+        iteration_list : list[dict], default=[]
+            Build a seperate bash file based on each entry in this list. The entries are command line arguments. An
+            entry should be of type `dict` and look as follows::
+
+                entry = {
+                    "name": "seeds",  # string
+                    "id": "s",  # string
+                    "values": [0, 1, 2, 3, 4]   # list; elements must be convertible to string
+                }
+
+            This example entry would generate 5 scripts with ' --seeds entry["values"][i]' appended to the basecommand.
+            The "id" field is used to add the information to the filename. For seed 3 this would add "_s3" to the file-
+            name and job name.
+
+        """
         self.mail_user = mail_user
         self.base_command = base_command
         self.mail_type = mail_type
@@ -49,28 +115,74 @@ class SlurmBuilder(object):
         self.shfilenames = []
 
     def build_slurm_header(self, job_name_identifier: str = ""):
+        """
+        Build slurm header specifying slurm options.
+
+        Parameters
+        ----------
+        job_name_identifier : str, default=""
+            Is appended to job_name.
+
+        Returns
+        -------
+        header : str
+            slurm header with slurm options
+
+        """
         header = ""
         header += "#!/bin/bash\n"
         header += "#\n"
 
         for command_name, command_value in self.slurm_config.items():
-            if command_name == "job-name":
+            if command_name == "job-name" and len(job_name_identifier) > 0:
                 command_value += f"_{job_name_identifier}"
             header += self.template_slurm_config.format(command_name=command_name, command_value=command_value)
         header += "\n"
         return header
 
     def build_precommands(self):
+        """
+        Build precommands.
+
+        Returns
+        -------
+        str
+            precommands
+        """
         commands = ""
         commands += self.pre_command
         return commands
 
     def build_postcommands(self):
+        """
+        Build postcommands.
+
+        Returns
+        -------
+        str
+            postcommands
+        """
         commands = ""
         commands += self.post_command
         return commands
 
     def build_shfilename(self, job_name_identifier: str = ""):
+        """
+        Build bash filenames.
+
+        Format of filename: `f"{self.runscript_outdir}/run_{self.job_name}_{job_name_identifier}.sh"`
+
+        Parameters
+        ----------
+        job_name_identifier : str, default=""
+            Job name id specific to run.
+
+        Returns
+        -------
+        str
+            bash filename
+
+        """
         return f"{self.runscript_outdir}/run_{self.job_name}_{job_name_identifier}.sh"
 
     def build_spawn_command(self, shfilename: str, to_args: bool = True):
@@ -90,9 +202,6 @@ class SlurmBuilder(object):
         with open(self.runlist_fname, 'w') as file:
             file.write(content)
 
-    def build_job_name_identifier_postfix(self, **kwargs):
-        raise NotImplementedError
-
     def spawn(self):
         for shfilename in self.shfilenames:
             spawncommand = self.build_spawn_command(shfilename=shfilename)
@@ -100,13 +209,48 @@ class SlurmBuilder(object):
             print(f"Spawned {spawncommand}")
 
     def build_shfile_body(self, job_name_identifier, **kwargs):
+        """
+        Build complete body of bash file.
+
+        Header, precommands, basecommand with optional command line arguments, postcommands.
+
+        Parameters
+        ----------
+        job_name_identifier : str, default=""
+            Job name id specific to run.
+        kwargs : dict
+            Keyword arguments that are going be rendered as command line arguments for the basecommand as follows:
+            `f"--{key} {value}"`.
+
+        Returns
+        -------
+        str
+            Formatted content of the bash file.
+
+        """
         header = self.build_slurm_header(job_name_identifier=job_name_identifier)
         precommands = self.build_precommands()
         maincommand = self.build_maincommand(**kwargs)
-        fullcommand = header + precommands + maincommand
+        postcommands = self.build_postcommands()
+        fullcommand = header + "\n" + precommands + "\n" + maincommand + "\n" + postcommands
         return fullcommand
 
     def build_maincommand(self, **kwargs):
+        """
+        Build main command which is the `base_command` with optional command line arguments.
+
+        Parameters
+        ----------
+        kwargs : dict
+            Keyword arguments that are going be rendered as command line arguments for the basecommand as follows:
+            `f"--{key} {value}"`.
+
+        Returns
+        -------
+        str
+            main command
+
+        """
         maincommand = ""
         maincommand += self.base_command
         for key, val in kwargs.items():
@@ -114,6 +258,17 @@ class SlurmBuilder(object):
         return maincommand
 
     def build_shfiles(self):
+        """
+        Build all bash files and save them to given output directory.
+
+        Build all possible combinations of values of `iteration_list` as single bash files. A file `runcommands.sh` is
+        written to the output directory containing all commands to slurmstart all single bash files.
+
+        Returns
+        -------
+        None
+
+        """
         arg_names = [v["name"] for v in self.iteration_list]
         arg_ids = [v["id"] for v in self.iteration_list]
         arg_vals = [v["values"] for v in self.iteration_list]
@@ -147,7 +302,9 @@ if __name__ == "__main__":
     sbuilder = SlurmBuilder(
         job_name="my_slurm_job",
         mail_user="some_mail@bubbib.com",
+        pre_command="echo setting env",
         base_command="echo Hello World",
+        post_command="echo cleaning up",
         iteration_list=[
             {
                 "name": "seeds",
